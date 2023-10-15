f5a2c4981e2d73ea81d1419b395c802578b81c87
Hiếu Huỳnh <mike.beplus@gmail.com>
2023-10-15 14:48:02 +0700

    Update npm

--- project-pack/src/js/salesforce/admin/sf-event-import/components/PopoverBox.js
+++ project-pack/src/js/salesforce/admin/sf-event-import/components/PopoverBox.js
@@ -1,17 +1,13 @@
 import {
-  Popover,
-  Trigger,
   Target,
-  usePlacement,
-  useIsOpen,
-  Close
+  useIsOpen
 } from "@accessible/popover";
 
 export default function PopoverBox({ placement = "right", label, children }) {
   const isOpen = useIsOpen();
 
   return (
-    <Target placement={ placement }>
+    <Target placement={ placement }> 
       <div className={ ['popover-box', isOpen ? '__open' : ''].join(' ') } >
         {
           label ? <label className="__label">{ label }</label> : ''
